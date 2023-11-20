66eab05834f2159b623ac621b5327c51ebed608a
nigel7huyc <nigel7huyc@163.com>
2023-11-20 14:17:47 +0800

    [MOD] the response content

--- apps/app/api/api_io.py
+++ apps/app/api/api_io.py
@@ -1,7 +1,8 @@
+import os
 from flask import Flask, jsonify, request
 
 from utils.utils_log import LogFactory
-from model.functional import CallResNet
+from model.Functional import CallResNet, ImageInteract
 
 app_server = Flask(__name__)
 
@@ -9,12 +10,13 @@ log = LogFactory.get_log('audit')
 
 
 def encrypt_resnet(img_path):
-    call_resnet = CallResNet(img_path)
-    random_seed = call_resnet.initialized_seed()
-    encrypt_period, encrypt_plot_path = call_resnet.encrypt_fig(random_seed)
-    entropy, params = call_resnet.cal_entropy()
-    decrypt_period, decrypt_plot_path = call_resnet.decrypt_fig(random_seed)
-    performance_plot_path = call_resnet.estimate_performance(encrypt_period, decrypt_period, entropy, params)
+    call_resnet = CallResNet()
+    image_interact_tool = ImageInteract(img_path)
+    random_figure = call_resnet.random_image_generator()
+    encrypt_period, encrypt_plot_path = image_interact_tool.encrypt_fig(random_figure)
+    decrypt_period, decrypt_plot_path = image_interact_tool.decrypt_fig(random_figure)
+    entropy = image_interact_tool.cal_entropy()
+    performance_plot_path = image_interact_tool.plot_performance_indicator(encrypt_period, decrypt_period, entropy)
     return encrypt_plot_path, decrypt_plot_path, performance_plot_path
 
 
@@ -27,8 +29,11 @@ def encrypt():
     img.save(img_path)
     log.info("[encrypt] save the image to img_path: {}".format(img_path))
     encrypt_path, decrypt_path, performance_path = encrypt_resnet(img_path)
+    host_ip = os.getenv('HOST_IP', '0.0.0.0')
+    base_url = 'http://{}:8099'.format(host_ip)
     log.info("[encrypt] encrypt figure path: {}, performance figure path: {}".format(encrypt_path, performance_path))
     return jsonify({
-        'encrypt_fig': 'http://127.0.0.1:8099{}'.format(encrypt_path),
-        'performance_plot': 'http://127.0.0.1:8099{}'.format(performance_path)
+        'encrypt_fig': base_url+'{}'.format(encrypt_path),
+        'decrypt_fig': base_url+'{}'.format(decrypt_path),
+        'performance_plot': base_url+'{}'.format(performance_path)
     })
